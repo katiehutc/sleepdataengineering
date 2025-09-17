a5cc02a8aa9f2a8c6dd9345ac4d9d8bfad6d992d
TrevorXu-hub <pxg6af@virginia.edu>
2025-09-17 12:03:56 +0200

    Create split_data_new.py

---
+++ split_data_new.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv("owid-covid-data.csv")
+for continent, group_df in df.groupby('continent'):
+    filename = f"{continent}.csv"
+    group_df.to_csv(owid-covid-data.csv, index=False)
